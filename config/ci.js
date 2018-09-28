bd9a6e3d686278736bbb37a8e1de775d2842e76c
pabloabadsanz <pabloabadsanz@gmail.com>
2018-09-28 20:22:26 +0200

    Travis test

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '319180065996-game84pt4vad7js31ilea9c0vbl0fqd6.apps.googleusercontent.com',
+  googleClientSecret: 'F3VxbDWuE_W26V4zU2WyDnAs',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
